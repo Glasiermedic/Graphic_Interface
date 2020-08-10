00a192847e3e3915da563146e165ce65d43a2468
Glasiermedic <wrolfson@glasierstudios.com>
2020-08-09 20:03:33 -0700

    Update feedback.py
    added in a full response to clicking submission button and a message box to acknowledge the entry

--- feedback.py
+++ feedback.py
@@ -50,15 +50,16 @@ class Feedback:
         self.entry_email = ttk.Entry(self.frame_content, width = 24).grid(row = 1, column = 1, padx = 5)
         self.text_comments = Text(self.frame_content, width = 40, height = 10, ).grid(row = 3, column = 0, columnspan=2, padx = 5)
 
-        ttk.Button(self.frame_content, text ='Submit', self.submit).grid(row = 4, column = 0, padx = 5, sticky = 'e')
-        ttk.Button(self.frame_content, text = 'Clear', self.clear).grid(row = 4, column = 1, padx = 5, sticky = 'w')
+        ttk.Button(self.frame_content, text ='Submit', command = self.submit).grid(row = 4, column = 0, padx = 5, sticky = 'e')
+        ttk.Button(self.frame_content, text = 'Clear', command = self.clear).grid(row = 4, column = 1, padx = 5, sticky = 'w')
 
     def submit(self):
-        print('Thank you {}, \n We got your submission and will send a copy to {}. \
-            Your submission was as follows: \n \
-            {}' .format(self.entry_name.get(), 
-            self.entry_email.get(), 
-            self.text.comments.get()))
+        print('Name: {}'.format(self.entry_name.get()))
+        print('Email: {}'.format(self.entry_email.get()))
+        print('Name: {}'.format(self.text_comments.get(1.0,'end')))
+        self.clear()
+        
+
 
     def clear(self):
         self.entry_name.delete(0,'end')
